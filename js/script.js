c4ef442b69535ffd6180ab192519c16b48eb403c
Julian Wenger <julian.wenger@bucher-suter.com>
2026-05-29 10:54:15 +0200

    feat: Interaktives JS-Gadget mit Canvas und IPO-Prinzip hinzugefügt

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ function checkLogin() {
     }
     window.location.href = pw + ".html";
 }
+
+function drawCircle() {
+    let radius = parseInt(document.getElementById("radiusInput").value);
+    if (radius < 10) radius = 10;
+    if (radius > 150) radius = 150;
+    
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "#0056b3";
+    ctx.fill();
+    
+    document.getElementById("gadgetOutput").innerText = "Kreis gezeichnet: " + radius + "px";
+}
